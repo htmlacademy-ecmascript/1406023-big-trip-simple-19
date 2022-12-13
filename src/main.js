a2fedf76055b8949b25b287fad30718af2cdd434
Ilya <ilya.nikolenko.job@gmail.com>
2022-12-13 17:51:46 +0300

    Добавляет адаптеры, класс для работы с АПИ

--- src/main.js
+++ src/main.js
@@ -3,3 +3,51 @@ import './view/sort-view/sort-view';
 import './view/list-view/list-view';
 import './view/point-view/point-view';
 import './view/new-point-editor-view/new-point-editor-view';
+import Store from './store';
+
+const BASE = 'https://19.ecmascript.pages.academy/big-trip-simple';
+const AUTH = 'Basic frd23sjd1ade';
+
+/**
+ * @type {Store<Point>}
+ */
+const pointsStore = new Store(`${BASE}/points`, AUTH);
+/**
+ * @type {Store<Destination>}
+ */
+const destinationsStore = new Store(`${BASE}/destinations`, AUTH);
+/**
+ * @type {Store<OfferGroup>}
+ */
+const offerGroupsStore = new Store(`${BASE}/offers`, AUTH);
+
+
+pointsStore.list().then(async (items) => {
+  const {log} = console;
+
+  log('Points: List', items);
+
+  const date = new Date().toJSON();
+  const item = await pointsStore.add({
+    'base_price': 100,
+    'date_from': date,
+    'date_to': date,
+    'destination': 1,
+    'offers': [],
+    'type': 'bus'
+  });
+
+  log('Points: Add', item);
+
+  item['base_price'] = 200;
+  log('Points: Update', await pointsStore.update(item));
+
+  log('Points: Delete', await pointsStore.delete(item.id));
+
+  log('Destinations: List', await destinationsStore.list());
+
+  log('offerGroupsStore: List', await offerGroupsStore.list());
+
+});
+
+
