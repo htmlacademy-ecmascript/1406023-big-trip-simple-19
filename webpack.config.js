3a4f80f28fc211f802591f2a3403188f4e90b212
Ilya <ilya.nikolenko.job@gmail.com>
2023-01-30 19:33:18 +0300

    Добавляет импорт TerserPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/main.js',
